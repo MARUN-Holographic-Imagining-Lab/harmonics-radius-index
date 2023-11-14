dde1c69364f2102d33680e5fe01ca7ac3e4bc775
Gyokhan Kochmarla <gokhan.kocmarli@gmail.com>
2023-11-14 20:43:39 +0300

    feat: last code

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from core.analyze import SuperResolutionAnalyzer
 from core.image_preperation_factory import ImagePossibleTypes
 
-analyzer = SuperResolutionAnalyzer("inji_hr.png", scale=8)
+analyzer = SuperResolutionAnalyzer("test_images/sehir_hr.png", scale=8)
+
 analyzer.show_image("all", domain="spatial")
 analyzer.show_image("all", domain="frequency")
 
-for possible_type in ImagePossibleTypes:
-    if possible_type != ImagePossibleTypes.SMALL:
-        analyzer.remove_components_below(possible_type, threshold=7)
+# Different Thresholds for Bicubic Interpolation
+for threshold in range(1, 13):
+    analyzer.remove_components_below(ImagePossibleTypes.BICUBIC, threshold=threshold)
+
+for threshold in [9, 7]:
+    for possible_type in ImagePossibleTypes:
+        if possible_type != ImagePossibleTypes.SMALL:
+            analyzer.remove_components_below(possible_type, threshold=threshold)
